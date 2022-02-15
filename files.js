2fa2f6ec3e7c274c328b1cdfb6b9cb1d36ac413e
Sanjay R <sanjayprashanth1997@gmail.com>
2022-02-15 19:30:07 +0530

    initial commit

---
+++ files.js
@@ -0,0 +1,77 @@
+db.mentors.insertMany([
+    {
+      id: "1",
+      name: "Lavish",
+    },
+    {
+      id: "2",
+      name: "Ragav",
+    },
+    {
+      id: "3",
+      name: "Zeel",
+    },
+    {
+      id: "4",
+      name: "Zeim",
+    },
+    {
+      id: "5",
+      name: "Akbar",
+    },
+    {
+      id: "6",
+      name: "Basheer",
+    }
+  ])
+  
+  db.students.insertMany([
+      {
+        id: "1",
+        name: "Sanjay",
+      },
+      {
+        id: "2",
+        name: "Saravana",
+      },
+      {
+        id: "3",
+        name: "Zavier",
+      },
+      {
+        id: "4",
+        name: "Zaheer",
+      },
+      {
+        id: "5",
+        name: "Zainab",
+      },
+      {
+        id: "6",
+        name: "VIgnesh",
+      },
+      {
+          id: "7",
+          name: "stephen",
+        },
+        {
+          id: "8",
+          name: "Samvel",
+        },
+        {
+          id: "9",
+          name: "francis",
+        },
+        {
+          id: "10",
+          name: "farnklin",
+        },
+        {
+          id: "11",
+          name: "joel",
+        },
+        {
+          id: "12",
+          name: "malik",
+        },
+    ])
---
+++ index.js
@@ -0,0 +1,88 @@
+// const express = require('express');
+// const {MongoClient} = require ('mongodb')
+import express from "express";
+import { MongoClient } from "mongodb";
+const app = express();
+const PORT = 3200;
+
+const MONGO_URL ="mongodb://localhost";
+
+async function createConnection(){
+    const client = new MongoClient(MONGO_URL)
+    await client.connect();
+    console.log("Mongo is connected") //checking wheter mongo is connected 
+    return client;
+
+}
+
+const client = await createConnection(); 
+
+app.use(express.json());
+
+app.get("/mentors",   async (req,res)=>{
+    console.log(req.query)
+    const mentor =  await client
+    .db("mentorstudent")
+    .collection("mentors")
+    .find(req.query)
+    .toArray();
+    res.send(mentor);
+});
+
+
+app.get("/mentors/:id",  async (req,res)=>{
+    const{id}= req.params;
+    const mentor = await client.db("mentorstudent").collection("mentors").findOne({id:id});
+    mentor ? res.send(mentor) : res.status(404).send({message:"No Mentors Found On this Id"});
+});
+
+app.delete("/mentors/:id",  async (req,res)=>{
+    const{id}= req.params;
+    const mentor = await client.db("mentorstudent").collection("mentors").deleteOne({id:id});
+    mentor ? res.send(mentor) : res.status(404).send({message:"No Mentors Found On this Id"});
+});
+
+app.post("/mentors", async (req,res)=>{
+    const newMentor = req.body;
+    console.log(req.body)
+    const postedMentor = await client
+    .db("mentorstudent")
+    .collection("mentors")
+    .insertMany(newMentor);
+    res.send(postedMentor);
+});
+
+
+app.get("/students", async (req,res)=>{
+    console.log(req.query);
+    const student = await client
+    .db("mentorstudent")
+    .collection("students")
+    .find(req.query)
+    .toArray();
+    res.send(student);
+});
+
+app.post("/students", async (req,res)=>{
+    const newStudent = req.body;
+    const postedStudent = await client
+    .db("mentorstudent")
+    .collection("students")
+    .insertMany(newStudent);
+    res.send(postedStudent);
+});
+
+
+app.get("/students/:id",  async (req,res)=>{
+    const{id}= req.params;
+    const student = await client.db("mentorstudent").collection("students").findOne({id:id});
+    res.send(student);
+});
+
+app.delete("/students/:id",  async (req,res)=>{
+    const{id}= req.params;
+    const student = await client.db("mentorstudent").collection("students").deleteOne({id:id});
+    res.send(student);
+});
+
+app.listen(PORT, ()=>console.log(`Server Started in localhost:${PORT} `)); 
